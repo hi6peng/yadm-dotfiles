d5bb2ddf5b47d7a241411580eb06e1918d1197a4
6peng <6peng@proton.me>
2022-05-18 21:45:37 +0800

    add some env, fun from Manjaro

--- .sh/inc/00-env.sh
+++ .sh/inc/00-env.sh
@@ -150,6 +150,10 @@ pathedit -a "/usr/sbin"
 
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
+
 export ALIYUNPAN_CONFIG_DIR=$HOME/.config/aliyunpan
 
 # vim:filetype=sh:tabstop=2:shiftwidth=2:fdm=marker:
